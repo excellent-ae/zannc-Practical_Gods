312de3c35092c3ca58c02f9b8b42551aac161730
zanncdwbl <xspelz@gmail.com>
2025-12-03 14:45:21 +0000

    update to fix issues, add images etc, functional but incomplete.

--- src/main.lua
+++ src/main.lua
@@ -1,72 +1,50 @@
 ---@meta _
-zanncdwbl_Practical_Gods = zanncdwbl_Practical_Gods or {}
 
 ---@diagnostic disable-next-line: undefined-global
 local mods = rom.mods
 
 ---@diagnostic disable: lowercase-global
----@module "SGG_Modding-ENVY-auto"
-mods["SGG_Modding-ENVY"].auto()
+---@module 'LuaENVY-ENVY-auto'
+mods["LuaENVY-ENVY"].auto()
 
 ---@diagnostic disable-next-line: undefined-global
 rom = rom
 ---@diagnostic disable-next-line: undefined-global
 _PLUGIN = PLUGIN
 
----@module "SGG_Modding-Hades2GameDef-Globals"
 game = rom.game
 
----@module "SGG_Modding-SJSON"
-sjson = mods["SGG_Modding-SJSON"]
-
----@module "SGG_Modding-ModUtil"
 modutil = mods["SGG_Modding-ModUtil"]
 
----@module 'SGG_Modding-Chalk'
 chalk = mods["SGG_Modding-Chalk"]
-
----@module "SGG_Modding-ReLoad"
 reload = mods["SGG_Modding-ReLoad"]
+sjson = mods["SGG_Modding-SJSON"]
+gods = mods["zannc-GodsAPI"].auto()
 
 ---@module 'Practical_Gods-zannc-config'
 config = chalk.auto("config.lua")
 public.config = config
 
+import_as_fallback(rom.game)
+
 local function on_ready()
-	import_as_fallback(rom.game)
+	local package = rom.path.combine(_PLUGIN.plugins_data_mod_folder_path, _PLUGIN.guid)
+	modutil.mod.Path.Wrap("SetupMap", function(base)
+		LoadPackages({ Name = package })
+		base()
+	end)
 
-	import("ready.lua")
-	import("sjson.lua")
-
-	-- Hermes Boons
-	import("hermes/speedDamage.lua")
-	import("hermes/moveSpeed.lua")
-	import("hermes/dashHeal.lua")
-	import("hermes/bonusDash.lua")
-
-	-- Artemis Boons
-	import("artemis/artemisLootData.lua")
-	import("artemis/artemisSJSONIcons.lua")
-
-	import("artemis/weaponAttack/critAttack.lua")
-	import("artemis/weaponSpecial/critSpecial.lua")
-	import("artemis/critDamage/critDamage.lua")
-	import("artemis/armourDamage/armourDamage.lua")
-	import("artemis/sprintDamage/sprintDamage.lua")
-
-	-- import "artemis/weaponAttack/critAttackSJSON.lua"
-	-- import "artemis/weaponSpecial/critSpecialSJSON.lua"
-
-	import("artemis/artemisCritScaling.lua")
+	import("boons/artemisBoons.lua")
+	import("boons/hermesBoons.lua")
+	import("boons/athenaBoons.lua")
+	import("boons/dioBoons.lua")
 end
 
-local function on_reload()
-	import_as_fallback(rom.game)
-	import("imgui.lua")
-end
+local function on_reload() end
 
 local loader = reload.auto_single()
 
 modutil.once_loaded.game(function()
+	mod = modutil.mod.Mod.Register(_PLUGIN.guid)
 	loader.load(on_ready, on_reload)
 end)
